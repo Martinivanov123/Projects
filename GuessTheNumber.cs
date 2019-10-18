f2af3fc0dd2f06ffa8b8edbc249e8af6ae093e88
Martinivanov123 <43958330+Martinivanov123@users.noreply.github.com>
2019-10-19 00:04:30 +0300

    Add files via upload

---
+++ GuessTheNumber.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            int score = 0;
+
+            while (true)
+            {
+                score += Game();
+                Console.WriteLine("Score: {0}", score);
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to play again? Answer with yes or no");
+                string answer = Console.ReadLine();
+
+                if (answer == "no")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Final score: {0}", score);
+                    Console.WriteLine("Goodbye!");
+                    Console.WriteLine();
+                    break;
+                }
+
+                while (answer != "yes" && answer != "no")
+                {
+                    Console.WriteLine("Invalid answer! Please, answer with yes or no!");
+                    answer = Console.ReadLine();
+                }
+            }
+        }
+
+        static int Game()
+        {
+            Menu();
+
+            int level = int.Parse(Console.ReadLine());
+            int sanctions = 0;
+            bool win = false;
+
+            while (level < 1 || level > 3)
+            {
+                Console.WriteLine("The level you want to play does not exist. Try again!");
+                Console.Write("Please, enter your level: ");
+                level = int.Parse(Console.ReadLine());
+            }
+
+            int max = GetMaxNum(level);
+            int rand = GenNum(max);
+            int attempts = returnAttempts(level);
+
+            Console.WriteLine("WELCOME TO THE GAME! YOU HAVE {0} ATTEMPTS", attempts);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            while (attempts > 0 && !win)
+            {
+                Console.Write("Enter your number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                while (number < 0 || number > max)
+                {
+                    Console.Write("The allowed range is [{0} - {1}]! Please, enter again: ", 0, max);
+                    sanctions++;
+                    number = int.Parse(Console.ReadLine());
+                }
+
+                if (CorrectGuess(number, rand))
+                {
+                    Console.WriteLine("CONGRATULATIONS! YOU WON THE GAME!!!!1!!11!!11!!");
+                    win = true;
+                }
+
+                else
+                {
+                    attempts--;
+                    if (attempts > 0)
+                    {
+                        Console.WriteLine("WRONG!!! YOU HAVE {0} ATTEMPTS LEFT", attempts);
+                        if (rand < number)
+                        {
+                            Console.WriteLine("The guess is higher");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("The guess is lower");
+                        }
+                    }
+
+                    else if (attempts == 0)
+                    {
+                        Console.WriteLine("GAME OVER!!! YOU LOST! THE NUMBER WAS {0}!", rand);
+                    }
+
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            return Result(attempts, sanctions);
+        }
+
+        static int Result(int attempts, int sanctions)
+        {
+            int result = attempts * 10 - sanctions * 5;
+            return result;
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("====================================");
+            Console.WriteLine("========= GUESS THE NUMBER =========");
+            Console.WriteLine("====================================");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("PLEASE SELECT LEVEL:");
+            Console.WriteLine();
+            Console.WriteLine("1. LEVEL 1 - Guess a number from 0 to 10 in 3 attempts (1)");
+            Console.WriteLine("2. LEVEL 2 - Guess a number from 0 to 20 in 6 attempts (2)");
+            Console.WriteLine("3. LEVEL 3 - Guess a number from 0 to 30 in 9 attempts (3)");
+            Console.WriteLine();
+            Console.Write("Please, enter your level: ");
+        }
+
+        static int GenNum(int end)
+        {
+            Random rand = new Random();
+            return rand.Next(end);
+        }
+
+        static int GetMaxNum(int level)
+        {
+            int max = level * 10;
+            return max;
+        }
+
+        static int returnAttempts(int level)
+        {
+            int attempts = level * 3;
+            return attempts;
+        }
+
+        static bool CorrectGuess(int number, int rand)
+        {
+            if (number == rand)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ NeSeSyrdiChoveche.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace neSeSyrdi
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] players = new int[4];
+            int dice = 0;
+            int positions = 40;
+            bool winner = false;
+
+            int[] cursedPositions = new int[] { 13, 24, 33};
+            int[] blessedPositions = new int[] { 7, 21, 27};
+
+            while (!winner)
+            {
+                for (int i = 0; i < players.Length; i++)
+                {
+                    int currentPlayer = i + 1;
+                    
+                    Console.WriteLine("[PLAYER {0}] Rolling the dice...", currentPlayer);
+                    dice = RollingDice();
+                    Console.WriteLine("[PLAYER {0}] rolled {1}", currentPlayer, dice);
+
+                    if (dice != 6 && players[i] == 0)
+                    {
+                        Console.WriteLine("[PLAYER {0}] cannot move! They need a 6 from a dice!", currentPlayer);
+                    }
+
+                    else
+                    {
+                        if (players[i] + dice <= positions)
+                        {
+                            players[i] += dice;
+                            Console.WriteLine("[PLAYER {0}] after rolling the dice: {1}", currentPlayer, players[i]);
+
+                            for (int j = 0; j < 3 && dice == 6 && players[i] + dice <= positions; j++)
+                            {
+                                Console.WriteLine("[PLAYER {0}] has to roll the dice again!", currentPlayer);
+                                dice = RollingDice();
+                                Console.WriteLine("[PLAYER {0}] rolled {1}", currentPlayer, dice);
+                                players[i] += dice;
+                                Console.WriteLine("[PLAYER {0}] after rolling the dice: {1}", currentPlayer, players[i]);
+                            }
+
+                            for (int j = 0; j < players.Length; j++)
+                            {
+                                if (players[i] == players[j] && i != j)
+                                {
+                                    players[j] = 0;
+                                    Console.WriteLine("[PLAYER {0}] returned to the start position", j + 1);
+                                }
+                            }
+
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("[PLAYER {0}] cannot move. They need {1} to finish.", currentPlayer, positions - players[i]);
+                        }
+                    }
+
+
+                    for (int j = 0; j < cursedPositions.Length && j < blessedPositions.Length; j++)
+                    {
+                        if (players[i] == blessedPositions[j])
+                        {
+                            players[i] += 3;
+                            Console.WriteLine("[PLAYER {0}] just got on a blessed position. Goes to position {1}", currentPlayer, players[i]);
+                        }
+
+                        if (players[i] == cursedPositions[j])
+                        {
+                            players[i] -= 5;
+                            Console.WriteLine("[PLAYER {0}] stepped on a bomb position. Returns to position {1}", currentPlayer, players[i]);
+                        }
+                    }
+
+                    Console.WriteLine();
+
+                    if (players[i] == positions)
+                    {
+                        Console.WriteLine("[PLAYER {0}] IS THE WINNER", currentPlayer);
+                        Console.WriteLine();
+                        for (int j = 0; j < players.Length; j++)
+                        {
+                            int currentPlayer1 = j + 1;
+                            Console.WriteLine("[PLAYER {0}] position: {1}", currentPlayer1, players[j]);
+                        }
+
+                        winner = true;
+                        break;
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        static int RollingDice()
+        {
+            Random number = new Random();
+            return number.Next(1, 7);
+        }
+    }
+}
